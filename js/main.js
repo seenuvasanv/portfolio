b6e7f44a86d2a9be6d5e591934fa664079c179f2
seenuvasanv <velseenu@gmail.com>
2019-07-15 10:35:18 +0530

    Updates

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// Select DOM Items
+
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuNav = document.querySelector(".menu-nav");
+const menuBranding = document.querySelector(".menu-branding");
+const navItems = document.querySelectorAll(".nav-item");
+
+// Initial State of Menu
+let showMenu = false;
+
+const toggleMenu = function() {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
+
+    // Set menu State
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuNav.classList.remove("show");
+    menuBranding.classList.remove("show");
+    navItems.forEach(item => item.classList.remove("show"));
+
+    // Set menu State
+    showMenu = false;
+  }
+};
+
+// menu click event..
+menuBtn.addEventListener("click", toggleMenu);
